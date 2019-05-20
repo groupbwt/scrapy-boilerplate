fdf8e21670b9b73b450639f46432983871ccb933
Kristobal Junta <junta.kristobal@gmail.com>
2019-05-20 17:03:01 +0300

    update HttpProxyMiddleware

--- src/middlewares/HttpProxyMiddleware.py
+++ src/middlewares/HttpProxyMiddleware.py
@@ -5,7 +5,7 @@ class HttpProxyMiddleware(object):
     def process_request(self, request, spider):
         proxy = spider.settings.get('PROXY')
         proxy_auth = spider.settings.get('PROXY_AUTH')
-        if proxy is not None:
+        if proxy is not None and len(proxy):
             if proxy_auth is not None:
                 request.headers['Proxy-Authorization'] = basic_auth_header(*proxy_auth.split(':'))
             request.meta['proxy'] = "http://{}".format(proxy)

d401a8f909e6f3d93ff53c5afb072db2a93cb1c6
Kristobal Junta <junta.kristobal@gmail.com>
2019-05-28 17:23:54 +0300

    update BaseProducer class

--- src/rabbitmq/__init__.py
+++ src/rabbitmq/__init__.py
@@ -1,5 +1,7 @@
 import os, sys
 
+from .BaseConsumer import BaseConsumer
+from .BaseProducer import BaseProducer
 from .PikaFactory import PikaFactory
 from .RabbitConnection import RabbitConnection
 

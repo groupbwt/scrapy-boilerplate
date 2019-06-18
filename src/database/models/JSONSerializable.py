f082d841cf8148c48ceb1a44f59e3ecdf7deb263
Kristobal Junta <junta.kristobal@gmail.com>
2019-06-18 11:48:16 +0300

    add base classes to database/models

---
+++ src/database/models/JSONSerializable.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class JSONSerializable:
+    def _serialize(self, value):
+        if type(value) not in (int, float, bool, type(None)):
+            return str(value)
+
+        return value
+
+    def as_dict(self):
+        return {c.name:self._serialize(getattr(self, c.name)) for c in self.__table__.columns}

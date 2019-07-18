b64ebdac577cffb21072eb5f24f75def3d2d082a
Kristobal Junta <junta.kristobal@gmail.com>
2019-07-18 14:14:57 +0300

    replace logger with prints; improve help & error messages

--- src/commands/new.py
+++ src/commands/new.py
@@ -6,41 +6,50 @@ import sys
 import inflection
 from mako.template import Template
 from scrapy.commands import ScrapyCommand
+from scrapy.exceptions import UsageError
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
 
 class NewCommand(ScrapyCommand):
-    def __init__(self, logger=None):
+    def __init__(self):
         super().__init__()
         self.settings = get_project_settings()
-        self.logger = logger or logging.getLogger(name=__name__)
 
-    def set_logger(self, name="COMMAND", level="DEBUG"):
-        self.logger = logging.getLogger(name=name)
-        self.logger.setLevel(level)
+    def syntax(self):
+        return "<template type> <camelcase class name>"
+
+    def short_desc(self):
+        return "Generate new class file from template"
 
     def add_options(self, parser):
         super().add_options(parser)
 
         parser.add_option(
-            '--rabbit',
+            "--rabbit",
             action="store_true",
             dest="use_rabbit",
             default=False,
+            help="add RabbitMQ code (works for some templates)",
+        )
+
+        parser.add_option(
+            "-d",
+            "--debug",
+            action="store_true",
+            dest="debug",
+            default=False,
+            help="enable debug output for this command",
         )
 
     def run(self, args, opts):
-        self.set_logger("new", self.settings.get("LOG_LEVEL"))
-        configure_logging()
-
         if len(args) < 2:
-            self.logger.critical("invalid args count")
-            sys.exit(1)
+            raise UsageError()
 
         SUPPORTED_TEMPLATE_TYPES = [
             name.split(".")[0] for name in os.listdir("_templates")
         ]
+
         DEST_PREFIXES = {
             "command": ["commands"],
             "extension": ["extensions"],
@@ -53,11 +62,10 @@ class NewCommand(ScrapyCommand):
         }
 
         template_type = args[0]
-        self.logger.debug(template_type)
 
         if template_type not in SUPPORTED_TEMPLATE_TYPES:
-            self.logger.critical("unsupported template type: %s", template_type)
-            self.logger.info("supported are: %s", repr(SUPPORTED_TEMPLATE_TYPES))
+            print(f"ERROR: unsupported template type: {template_type}")
+            print("supported types: {}".format(repr(SUPPORTED_TEMPLATE_TYPES)))
             sys.exit(1)
 
         template_name = os.path.join("_templates", "{}.py.mako".format(template_type))
@@ -74,7 +82,7 @@ class NewCommand(ScrapyCommand):
         file_path = os.path.join(*file_prefix, "{}.py".format(file_name))
 
         if os.path.exists(file_path):
-            self.logger.warning("file already exists")
+            print("WARNING: file already exists")
             do_overwrite = input("overwrite? [y/N] ")
 
             if do_overwrite.lower() not in ["y", "yes"]:
@@ -88,7 +96,7 @@ class NewCommand(ScrapyCommand):
             spider_name=spider_name,
             table_name=table_name,
             logger_name=logger_name,
-            use_rabbit=opts.use_rabbit
+            use_rabbit=opts.use_rabbit,
         )
 
         print(rendered_code)
@@ -118,4 +126,4 @@ class NewCommand(ScrapyCommand):
         init_file.write("\n")
         init_file.close()
 
-        self.logger.info("created %s %s", template_type, file_name)
+        print(f"created {template_type} {file_name}")

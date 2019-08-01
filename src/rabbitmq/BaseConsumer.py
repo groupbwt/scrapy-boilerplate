080250c4f69d0dfd74a8b777420ccab8fc52e8c2
Kristobal Junta <junta.kristobal@gmail.com>
2019-08-01 11:26:54 +0300

    remove old rabbitmq files

--- src/rabbitmq/BaseConsumer.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-class BaseConsumer(object):
-    def __init__(self):
-        pass
--- src/rabbitmq/__init__.py
+++ src/rabbitmq/__init__.py
@@ -1,6 +1,2 @@
 # -*- coding: utf-8 -*-
-from .BaseConsumer import BaseConsumer
-from .BaseProducer import BaseProducer
-from .PikaFactory import PikaFactory
-from .RabbitConnection import RabbitConnection
 from .RabbitSpider import RabbitSpider

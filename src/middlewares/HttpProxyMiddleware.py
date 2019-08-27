a9aa80751dacb0f70afa170df918e4d3ab844b1f
Kristobal Junta <junta.kristobal@gmail.com>
2019-08-27 10:07:03 +0300

    add PROXY_ENABLED setting

--- src/middlewares/HttpProxyMiddleware.py
+++ src/middlewares/HttpProxyMiddleware.py
@@ -3,7 +3,8 @@ from w3lib.http import basic_auth_header
 
 
 class HttpProxyMiddleware(object):
-    def process_request(self, request, spider):
+    @staticmethod
+    def update_request(request, spider):
         proxy = spider.settings.get("PROXY")
         proxy_auth = spider.settings.get("PROXY_AUTH")
         if proxy:
@@ -11,5 +12,16 @@ class HttpProxyMiddleware(object):
                 request.headers["Proxy-Authorization"] = basic_auth_header(
                     *proxy_auth.split(":")
                 )
+            if 'http' not in proxy:
+                proxy = "http://{}".format(proxy)
+            request.meta["proxy"] = proxy
+        return request
 
-            request.meta["proxy"] = "http://{}".format(proxy)
+    def process_request(self, request, spider):
+        if hasattr(spider, 'proxy_enabled'):
+            if not spider.proxy_enabled:
+                return
+            else:
+                request = HttpProxyMiddleware.update_request(request, spider)
+        elif spider.settings.get('PROXY_ENABLED'):
+            request = HttpProxyMiddleware.update_request(request, spider)

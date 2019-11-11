26631e866b048f3627657f401852a3df6532a697
dobrovolskyi_ha@groupbwt.com <dobrovolskyi_ha@groupbwt.com>
2019-11-11 15:32:51 +0200

    change rabbitmq interaction

--- src/rabbitmq/RabbitSpider.py
+++ src/rabbitmq/RabbitSpider.py
@@ -12,11 +12,20 @@ from scrapy.utils.project import get_project_settings
 
 
 class RabbitSpider:
-    def __init__(self, *args, **kwargs):
-        settings = get_project_settings()
-        self.rabbitmq_connect(settings)
+    """
+    Mixin to interact with RabbitMQ
+    """
+    # def __init__(self, *args, **kwargs):
+    #     self.settings = get_project_settings()
+    #     self.rabbitmq_connect(self.settings)
 
-    def rabbitmq_connect(self, settings):
+    def rmq_connect(self, settings):
+        """
+        connect to server
+        """
+        if not self.logger:
+            self.logger = logging.getLogger(settings.get('COMPOSE_PROJECT_NAME'))
+
         logging.getLogger("pika").setLevel(os.getenv("PIKA_LOG_LEVEL"))
 
         parameters = pika.ConnectionParameters(
@@ -30,25 +39,34 @@ class RabbitSpider:
             heartbeat=0,
         )
 
-        self.connection = pika.BlockingConnection(parameters)
+        self.rmq_delay = 30
+        self.rmq_connection = pika.BlockingConnection(parameters)
+        self.rmq_channel = self.rmq_connection.channel()
+        self.rmq_channel.basic_qos(prefetch_count=1)
 
-        self.channel = self.connection.channel()
-        self.channel.basic_qos(
-            prefetch_count=int(settings.get("CONCURRENT_REQUESTS", 1))
-        )
-
-    def prepare_request(self):
+    def rmq_preprocess_message(self, method, header_frame, body):
+        """
+        To rewrite, preprocess message before use it.
+        :return modified_method, modified_header_frame, modified_body
+        """
         raise NotImplementedError
 
-    def next_request(self):
-        queue_name = os.getenv("PUSHER_QUEUE", "")
-        while True:
-            stats = self.channel.queue_declare(queue=queue_name, durable=True)
-            if stats.method.message_count > 0:
-                method, header_frame, body = self.channel.basic_get(queue_name)
+    def rmq_next_message(self, queue_name):
+        stats = self.rmq_channel.queue_declare(queue=queue_name, durable=True)
+        if stats.method.message_count > 0:
+            method, header_frame, body = self.rmq_channel.basic_get(queue_name)
+            if body:
+                return self.rmq_preprocess_message(method, header_frame, body)
+        else:
+            return None, None, None
 
-                if body:
-                    return self.prepare_request(method, header_frame, body)
-            else:
-                self.logger.warning("No messages in the queue, waiting...")
-                time.sleep(30)
+    def rmq_get_message_count(self, queue):
+        """
+        Count message in the queue
+        """
+        res = self.rmq_channel.queue_declare(
+            queue=queue,
+            durable=True,
+            passive=True
+        )
+        return res.method.message_count

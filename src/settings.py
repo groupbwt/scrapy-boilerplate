cbee8a02dd6e1dfdef64cc0fe14ac77b302d2705
KristobalJunta <junta.kristobal@gmail.com>
2020-01-04 12:05:19 +0200

    update default request headers

--- src/settings.py
+++ src/settings.py
@@ -32,10 +32,7 @@ TELNETCONSOLE_PASSWORD = "password"
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
     "Cache-Control": "max-age=0",
 }
 

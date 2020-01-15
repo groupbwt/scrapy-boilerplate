9dc3a4fec60b6ea86b54aee784d819dcf60a20a3
gusv_aa <gusev_aa@groupbwt.com>
2020-01-15 18:10:33 +0200

    feat(helpers): add logger mixin

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -3,3 +3,4 @@ from .mysql_connection_string import mysql_connection_string
 from .pika_connection_parameters import pika_connection_parameters
 from .pika_blocking_connection import PikaBlockingConnection
 from .pika_select_connection import PikaSelectConnection
+from .logger_mixin import LoggerMixin

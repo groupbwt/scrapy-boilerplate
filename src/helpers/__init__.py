dbdf670f5ab78fb77e6b57ee74c9da6cec9654aa
gusv_aa <gusev_aa@groupbwt.com>
2020-01-15 14:39:50 +0200

    feat(helpers): add pika select connection class

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 from .mysql_connection_string import mysql_connection_string
 from .pika_connection_parameters import pika_connection_parameters
+from .pika_blocking_connection import PikaBlockingConnection
+from .pika_select_connection import PikaSelectConnection

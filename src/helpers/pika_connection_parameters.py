79f2b106b9df76b2ac8e9f371204861fd62cfbd0
gusv_aa <gusev_aa@groupbwt.com>
2020-01-15 14:14:08 +0200

    fix(helpers): change settings attr for pika connection parameters

--- src/helpers/pika_connection_parameters.py
+++ src/helpers/pika_connection_parameters.py
@@ -9,8 +9,8 @@ def pika_connection_parameters(settings=None):
     return pika.ConnectionParameters(
         host=settings.get('RABBITMQ_HOST'),
         port=int(settings.get('RABBITMQ_PORT')),
-        virtual_host=settings.get('RABBITMQ_VHOST'),
+        virtual_host=settings.get('RABBITMQ_VIRTUAL_HOST'),
         credentials=pika.credentials.PlainCredentials(
-            username=settings.get('RABBITMQ_USERNAME'), password=settings.get('RABBITMQ_PASSWORD')
+            username=settings.get('RABBITMQ_USER'), password=settings.get('RABBITMQ_PASS')
         ),
     )

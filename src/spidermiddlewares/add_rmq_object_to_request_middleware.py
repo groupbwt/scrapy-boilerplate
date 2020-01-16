35b159837f1f5d4cf72ab59856aabe3e3dd3fff8
gusv_aa <gusev_aa@groupbwt.com>
2020-01-16 16:00:19 +0200

    feat(spidermiddlewares): update add rmq object class
    (cherry picked from commit 80419c7a5c73cdf22e3a633a3bf0c23468488eb1)

--- src/spidermiddlewares/add_rmq_object_to_request_middleware.py
+++ src/spidermiddlewares/add_rmq_object_to_request_middleware.py
@@ -1,12 +1,28 @@
 import scrapy
+from scrapy.crawler import Crawler
+from scrapy.exceptions import NotConfigured
+
+from extensions import PikaBaseConsumer
 
 
 class AddRMQObjectToRequestMiddleware:
+    def __init__(self, crawler: Crawler):
+
+        for component in crawler.extensions.middlewares:
+            if isinstance(component, PikaBaseConsumer):
+                break
+        else:
+            raise NotConfigured
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        ext = cls(crawler)
+        return ext
+
     def process_spider_output(self, response, result, spider):
         for request_or_item in result:
             if isinstance(request_or_item, scrapy.Request):
-                if response.meta.get('rmq_object'):
-                    request_or_item.meta['rmq_object'] = response.meta.get('rmq_object')
+                request_or_item.meta['rmq_object'] = response.meta.get('rmq_object')
                 yield request_or_item
             else:
                 yield request_or_item

7e7f01e1397c7e7fe48b7c5fc3cc90a808d6eb29
KristobalJunta <junta.kristobal@gmail.com>
2020-01-30 15:11:43 +0200

    add model mixins for id, timestamps and status columns for mysql

---
+++ src/database/models/mixins/timestamps.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import text, Column
+from sqlalchemy.dialects.mysql import TIMESTAMP
+
+
+class MysqlTimestampsMixin:
+    created_at = Column(
+        "created_at",
+        TIMESTAMP,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+    )
+    updated_at = Column(
+        "updated_at",
+        TIMESTAMP,
+        nullable=False,
+        index=True,
+        unique=False,
+        server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
+        server_onupdate=text("CURRENT_TIMESTAMP"),
+    )

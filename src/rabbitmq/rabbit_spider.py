5086eed31a4184eea6c85521949766fdbc933c2d
kovtun_dv <kovtun_dv@groupbwt.com>
2020-03-23 15:57:10 +0200

    add inheritance of PikaBlockingConnection class

--- src/rabbitmq/rabbit_spider.py
+++ src/rabbitmq/rabbit_spider.py
@@ -1,44 +1,34 @@
 # -*- coding: utf-8 -*-
-import logging
 import os
 import time
 
-import pika
+from pika.spec import BasicProperties
+
+from scrapy import Request
 from scrapy.utils.project import get_project_settings
+from scrapy.settings import Settings
+
+from helpers import PikaBlockingConnection
 
 
-class RabbitSpider:
+class RabbitSpider(PikaBlockingConnection):
     def __init__(self, *args, **kwargs):
+        self.logger = None
+        self.channel = None
+        # will set .channel, .connection and .logger
+        super().__init__()
         self.rabbitmq_connect(get_project_settings())
 
-    def rabbitmq_connect(self, settings):
-        logging.getLogger("pika").setLevel(os.getenv("PIKA_LOG_LEVEL"))
+    def rabbitmq_connect(self, settings: Settings):
+        self.channel.basic_qos(prefetch_count=int(settings.get("CONCURRENT_REQUESTS", 1)))
 
-        parameters = pika.ConnectionParameters(
-            host=settings.get("RABBITMQ_HOST"),
-            port=settings.get("RABBITMQ_PORT"),
-            virtual_host=settings.get("RABBITMQ_VIRTUAL_HOST"),
-            credentials=pika.credentials.PlainCredentials(
-                username=settings.get("RABBITMQ_USER"),
-                password=settings.get("RABBITMQ_PASS"),
-            ),
-            heartbeat=0,
-        )
-
-        self.connection = pika.BlockingConnection(parameters)
-
-        self.channel = self.connection.channel()
-        self.channel.basic_qos(
-            prefetch_count=int(settings.get("CONCURRENT_REQUESTS", 1))
-        )
-
-    def prepare_request(self, method, header_frame, body):
+    def prepare_request(self, method, header_frame: BasicProperties, body: str) -> Request:
         raise NotImplementedError
 
-    def get_queue_name_from(self):
+    def get_queue_name_from(self) -> str:
         raise NotImplementedError  # example: return os.getenv("PUSHER_QUEUE", "")
 
-    def get_queue_name_to(self):
+    def get_queue_name_to(self) -> str:
         raise NotImplementedError  # example: return os.getenv("SAVER_QUEUE", "")
 
     def declare_queue_from(self):
@@ -47,7 +37,7 @@ class RabbitSpider:
     def declare_queue_to(self):
         return self.channel.queue_declare(queue=self.get_queue_name_to(), durable=True)
 
-    def next_request(self):
+    def next_request(self) -> Request:
         while True:
             stats = self.declare_queue_from()
             if stats.method.message_count:

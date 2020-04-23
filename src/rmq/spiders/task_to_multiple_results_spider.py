e25cebd0383f72ee20e9e29be5008c3a3f0dea49
KristobalJunta <junta.kristobal@gmail.com>
2020-04-23 12:04:37 +0300

    add rmq task_base_spider to keep things DRY

--- src/rmq/spiders/task_to_multiple_results_spider.py
+++ src/rmq/spiders/task_to_multiple_results_spider.py
@@ -1,32 +1,13 @@
-from rmq.spiders.httpbin_spider import HttpbinSpider
-from rmq.utils import TaskObserver, get_import_full_name
+# -*- coding: utf-8 -*-
 from rmq.extensions import RPCTaskConsumer
-from rmq.middlewares import TaskTossSpiderMiddleware, DeliveryTagSpiderMiddleware
+from rmq.spiders import TaskBaseSpider
+from rmq.utils import TaskObserver
 
 
-class TaskToMultipleResultsSpider(HttpbinSpider):
+class TaskToMultipleResultsSpider(TaskBaseSpider):
     name = "multiple"
 
     def __init__(self, *args, **kwargs):
         super(TaskToMultipleResultsSpider, self).__init__(*args, **kwargs)
         self.processing_tasks = TaskObserver()
         self.completion_strategy = RPCTaskConsumer.CompletionStrategies.REQUESTS_BASED
-
-    @classmethod
-    def update_settings(cls, settings):
-        spider_middlewares = settings.getdict('SPIDER_MIDDLEWARES')
-        spider_middlewares[get_import_full_name(TaskTossSpiderMiddleware)] = 140
-        spider_middlewares[get_import_full_name(DeliveryTagSpiderMiddleware)] = 150
-
-        spider_extensions = settings.getdict('EXTENSIONS')
-        spider_extensions[get_import_full_name(RPCTaskConsumer)] = 20
-
-        for custom_setting, value in (cls.custom_settings or {}).items():
-            if custom_setting == "SPIDER_MIDDLEWARES":
-                spider_middlewares = {**spider_middlewares, **value}
-            elif custom_setting == "EXTENSIONS":
-                spider_extensions = {**spider_extensions, **value}
-            else:
-                settings.set(custom_setting, value)
-        settings.set('SPIDER_MIDDLEWARES', spider_middlewares)
-        settings.set('EXTENSIONS', spider_extensions)

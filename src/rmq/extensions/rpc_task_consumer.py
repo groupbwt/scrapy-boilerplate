b9488d2e964d4098d92d57fd6dd8e31749b182c3
DmitryKovtun <dm_ko@outlook.com>
2020-06-05 13:21:55 +0300

    fix names of rmq user, pass and vhost settings

--- src/rmq/extensions/rpc_task_consumer.py
+++ src/rmq/extensions/rpc_task_consumer.py
@@ -101,8 +101,8 @@ class RPCTaskConsumer(object):
             port=int(self.__spider.settings.get("RABBITMQ_PORT")),
             virtual_host=self.__spider.settings.get("RABBITMQ_VHOST"),
             credentials=pika.credentials.PlainCredentials(
-                username=self.__spider.settings.get("RABBITMQ_USERNAME"),
-                password=self.__spider.settings.get("RABBITMQ_PASSWORD"),
+                username=self.__spider.settings.get("RABBITMQ_USER"),
+                password=self.__spider.settings.get("RABBITMQ_PASS"),
             ),
             heartbeat=RMQDefaultOptions.CONNECTION_HEARTBEAT.value,
         )

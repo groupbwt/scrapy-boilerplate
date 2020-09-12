ab0386a9bcd9407622471dce4c689f32f4982a88
Alex Yudin <alex.vulreid@gmail.com>
2020-09-12 14:26:10 +0300

    build(python): update poetry dependencies
    add sleep-fix for rabbitmq container setup script

--- docker/rabbitmq/setup.sh
+++ docker/rabbitmq/setup.sh
@@ -4,6 +4,8 @@ echo $RABBITMQ_USERNAME
 echo $RABBITMQ_VIRTUAL_HOST
 (
 count=0;
+# sleep to allow rabbitmq to start init flow and create necessary files in mounted dir
+sleep 10;
 # Execute list_users until service is up and running
 until timeout 5 rabbitmqctl list_users >/dev/null 2>/dev/null || (( count++ >= 60 )); do sleep 1; done;
 if rabbitmqctl list_users | grep guest > /dev/null

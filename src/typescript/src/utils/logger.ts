0b4792e7b59b9a0ccb588ca9fddd5d3df966492f
Gusev Anton <gusev_aa@groupbwt.com>
2021-01-15 09:39:30 +0200

    refactor(ts): refactor

--- src/typescript/src/utils/logger.ts
+++ src/typescript/src/utils/logger.ts
@@ -1,5 +1,6 @@
 import winston from "winston";
 import { Logger as LoggerInterface, format, level } from "winston";
+import { ConsoleTransportOptions, FileTransportOptions } from "winston/lib/winston/transports";
 
 export enum levels {
     ERROR = 0,
@@ -15,24 +16,18 @@ export class Logger {
     public static createLogger(name: string, level: levels): LoggerInterface {
         const stringLevel = levels[level].toLowerCase();
 
+        const transportAttributes: FileTransportOptions = {
+            filename: 'log.log',
+            level: stringLevel,
+            format: winston.format.combine(
+                winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+                this.scrapyLogFormat()
+            )
+        };
+
         const transport = !!process.env.LOG_FILE && process.env.LOG_FILE.length
-            ? new winston.transports.File({
-                // options: { flags: 'a', encoding: 'utf8' },
-                filename: 'log.log',
-                level: stringLevel,
-                format: winston.format.combine(
-                    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-                    this.scrapyLogFormat()
-                )
-            })
-            : new winston.transports.Console({
-                level: stringLevel,
-                format: winston.format.combine(
-                    winston.format.colorize(),
-                    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-                    this.scrapyLogFormat()
-                )
-            });
+            ? new winston.transports.File(transportAttributes)
+            : new winston.transports.Console(transportAttributes);
 
         const logger = winston.createLogger({
             level: stringLevel,

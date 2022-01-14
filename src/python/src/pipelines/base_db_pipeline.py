7288024fda5ee7afabe08c8f2baf326991c4f079
Oleksii Halimov <halimov_oa@groupbwt.com>
2022-01-14 14:39:29 +0200

    feat(pipeline): logging added

--- src/python/src/pipelines/base_db_pipeline.py
+++ src/python/src/pipelines/base_db_pipeline.py
@@ -1,5 +1,6 @@
+import logging
+
 from MySQLdb.cursors import DictCursor
-from scrapy import signals
 from scrapy.exceptions import NotConfigured
 from twisted.enterprise import adbapi
 from sqlalchemy.sql.base import Executable as SQLAlchemyExecutable
@@ -9,22 +10,21 @@ from sqlalchemy.dialects.mysql import insert
 class BaseDBPipeline:
     def __init__(self, project_settings):
         self.project_settings = project_settings
+        self.logger = logging.getLogger(BaseDBPipeline.__class__.__name__)
         self.db_connection_pool = None
-        self.db_api_name = 'MySQLdb'
+        self.db_api = 'MySQLdb'
         self.charset = 'utf8mb4'
         self.table = None
 
     @classmethod
     def from_crawler(cls, crawler):
-        pipeline = cls(crawler.settings)
-        crawler.signals.connect(pipeline.spider_opened, signal=signals.spider_opened)
-        return pipeline
+        return cls(crawler.settings)
 
-    def spider_opened(self):
+    def open_spider(self, spider):
         if not self.table:
             raise NotConfigured('Table attribute is not set for DB pipeline.')
         self.db_connection_pool = adbapi.ConnectionPool(
-            self.db_api_name,
+            self.db_api,
             host=self.project_settings.get("DB_HOST"),
             port=self.project_settings.getint("DB_PORT"),
             user=self.project_settings.get("DB_USERNAME"),
@@ -35,6 +35,7 @@ class BaseDBPipeline:
             cursorclass=DictCursor,
             cp_reconnect=True,
         )
+        self.logger.debug('Connection with database established.')
 
     def process_item(self, item, spider):
         self.db_connection_pool.runInteraction(self.save_item, item)
@@ -45,6 +46,7 @@ class BaseDBPipeline:
         if isinstance(stmt, SQLAlchemyExecutable):
             stmt_compiled = stmt.compile(compile_kwargs={'literal_binds': True})
             transaction.execute(str(stmt_compiled))
+            self.logger.debug('Item saved to database.')
 
     def build_insert_update_query_stmt(self, item):
         stmt = insert(self.table).values(**item)

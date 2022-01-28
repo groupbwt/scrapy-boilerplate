5f5a6318aba7942480bddcd0e2e71090c297607f
Oleksii Halimov <halimov_oa@groupbwt.com>
2022-01-28 12:31:02 +0200

    fix(pipeline): item early return to the next pipeline fixed

--- src/python/src/pipelines/base_db_pipeline.py
+++ src/python/src/pipelines/base_db_pipeline.py
@@ -2,25 +2,28 @@ import logging
 
 from MySQLdb.cursors import DictCursor
 from scrapy.exceptions import NotConfigured
+from sqlalchemy import Table
 from sqlalchemy.dialects import mysql
+from twisted.enterprise.adbapi import Transaction
+from twisted.internet.defer import inlineCallbacks
 from twisted.enterprise import adbapi
 from sqlalchemy.sql.base import Executable as SQLAlchemyExecutable
-from sqlalchemy.dialects.mysql import insert
+from sqlalchemy.dialects.mysql import insert, Insert
 
 
 class BaseDBPipeline:
     """Base pipeline for saving items direct to database,
     for correct work it must be inherited and its table property must be overridden"""
-    table = None
+    table: Table = None
 
     def __init__(self, project_settings):
+        if not self.table:
+            raise NotConfigured('Table attribute is not set for DB pipeline.')
         self.project_settings = project_settings
         self.logger = logging.getLogger(self.__class__.__name__)
         self.db_connection_pool = None
-        self.db_api = 'MySQLdb'
-        self.charset = 'utf8mb4'
-        if not getattr(self, 'table', None):
-            raise NotConfigured('Table attribute is not set for DB pipeline.')
+        self.db_api: str = 'MySQLdb'
+        self.charset: str = 'utf8mb4'
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -41,18 +44,20 @@ class BaseDBPipeline:
         )
         self.logger.debug('Connection with database established.')
 
+    @inlineCallbacks
     def process_item(self, item, spider):
-        self.db_connection_pool.runInteraction(self.save_item, item)
+        item = yield self.db_connection_pool.runInteraction(self.save_item, item)
         return item
 
-    def save_item(self, transaction, item):
+    def save_item(self, transaction: Transaction, item) -> None:
         stmt = self.build_insert_update_query_stmt(item)
         if isinstance(stmt, SQLAlchemyExecutable):
             stmt_compiled = stmt.compile(dialect=mysql.dialect(), compile_kwargs={'literal_binds': False})
-            transaction.execute(str(stmt_compiled), tuple(stmt_compiled.params.values()))
+            transaction.execute(str(stmt_compiled), list(stmt_compiled.params.values()))
             self.logger.debug('Item was successfully saved to database.')
+            return item
 
-    def build_insert_update_query_stmt(self, item):
+    def build_insert_update_query_stmt(self, item) -> Insert:
         """If there are fields not to be saved it is possible to override this method"""
         stmt = insert(self.table).values(**item)
         return stmt.on_duplicate_key_update(**stmt.inserted)

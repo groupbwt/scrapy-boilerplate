1dfa1b6c719192ac1ae9ff6bd1a36489e130d84d
Gusev Anton <gusev_aa@groupbwt.com>
2022-02-02 18:29:23 +0200

    feat(rmq): update rmq twisted

--- src/python/src/rmq_twisted/connections/twisted_connection.py
+++ src/python/src/rmq_twisted/connections/twisted_connection.py
@@ -3,7 +3,7 @@ from abc import ABC
 from typing import Any
 
 from pika.adapters import twisted_connection
-from pika.adapters.twisted_connection import ClosableDeferredQueue
+from pika.adapters.twisted_connection import ClosableDeferredQueue, TwistedProtocolConnection
 from pika.adapters.twisted_connection import TwistedChannel
 from scrapy.settings import Settings
 from twisted.internet import protocol, reactor, defer
@@ -18,6 +18,7 @@ class TwistedConnection(ABC):
     queue_object: ClosableDeferredQueue
     consumer_tag: str
     settings: Settings
+    connection: TwistedProtocolConnection
 
     def __init__(self, settings: Settings):
         self.settings: Settings = settings
@@ -36,21 +37,25 @@ class TwistedConnection(ABC):
         port = self.settings.get('RABBITMQ_PORT')
 
         deferred_connection: Deferred = cc.connectTCP(host, port)
-        deferred_connection.addCallback(lambda _protocol: _protocol.ready)
+        deferred_connection.addCallback(self.on_connected)
         deferred_connection.addCallback(self.__logging, f'{self.__class__.__name__} connected')
         deferred_connection.addCallback(self.run)
         deferred_connection.addErrback(self.twisted_errback)
         deferred_connection.addCallback(self.__logging, f'{self.__class__.__name__} run complete')
         return deferred_connection
 
+    def on_connected(self, _protocol: TwistedProtocolConnection) -> Deferred:
+        return _protocol.ready
+
     def twisted_errback(self, failure: Failure):
         self.logger.error(failure)
 
     @defer.inlineCallbacks
-    def run(self, connection) -> Deferred:
+    def run(self, connection: TwistedProtocolConnection) -> Deferred:
         """Receives the connection as parameter and then consumes from the queue
         periodically.
         """
+        self.connection = connection
         self.channel = yield connection.channel()
 
     def __logging(self, previous_value: Any, message: str) -> Any:

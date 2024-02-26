03a163c11d6b648335477902478669229bcbba05
Alex Yudin <yudin_an@groupbwt.com>
2024-02-26 10:24:15 +0200

    docs: update readme

--- docker/python/setup.sh
+++ docker/python/setup.sh
@@ -2,8 +2,8 @@
 set -eu
 cd /var/app/python/src
 poetry install
-poetry run alembic upgrade head
-pm2 status
+#poetry run alembic upgrade head
+#pm2 status
 # uncomment following line if pm2.config.js file existing in python/src dir and contain instructions
 # to run worker processes. Or replace with required bash terminal instructions
 #pm2 start pm2.config.js
